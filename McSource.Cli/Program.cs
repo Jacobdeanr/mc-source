bc95a8782ab664fd821d50a3bd18cd3bf9e1d232
Simon <h45h74x@gmail.com>
2022-09-10 00:22:31 +0200

    Added meta files

---
+++ McSource.Cli/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using McSource.Logging;
+
+namespace McSource.Cli
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      try
+      {
+        // var path = @"C:\Users\Simon\Documents\Userdata\SourceCraft\axis.schem";
+        var path = @"C:\Users\Simon\Documents\Userdata\SourceCraft\ttt_kekland.schem";
+        // var path = @"C:\Users\Simon\Documents\Userdata\SourceCraft\cobble_glowstone.schem";
+        // var path = @"C:\Users\Simon\Documents\Userdata\SourceCraft\cobble_glowstone_air.schem";
+        
+        Log.Info("Welcome to Converter. Starting up...");
+        new Converter().Convert(path, @"C:\Users\Simon\Documents\Userdata\SourceCraft\map.vmf");
+      }
+      catch (Exception e)
+      {
+        Log.Critical("An Exception occurred. Stopping...", e);
+      }
+
+      Console.WriteLine("Press any key to end the application...");
+      Console.ReadKey();
+    }
+  }
+}
